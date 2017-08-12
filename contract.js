a4310517c4c2dc5a39ac5cb6d0c4a57fa940cc01
Matthias Reining <matthias.reining@outlook.com>
2017-08-12 03:20:18 +0200

    add loops with components

--- contract.js
+++ contract.js
@@ -1,6 +1,19 @@
 
 function renderContract() {
     console.log('renderContract');
+
+    loadTemplate('new-contract.html', main);
+}
+
+function getAllRiskCalcData() {
+    let allRiskCalcData = [];
+    console.log( state.modelDef.riskCalcData);
+
+    Object.entries(state.modelDef.riskCalcData).map( ([riskKey, risk]) => {
+        console.log('riskKey');
+        console.log(riskKey);
+        allRiskCalcData.push(risk);
+    });
     
-    loadTemplate('administrative-data.html',document.getElementById('content'));
+    return allRiskCalcData;
 }
